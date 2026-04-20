8512e8e9ea68dab79455ad65de4df22933121fc5
Kimi Qin <440041588@gapps.yrdsb.ca>
2026-04-20 11:37:57 -0400

    Add change location

--- Hero.java
+++ Hero.java
@@ -1,11 +1,22 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import greenfoot.*;
 
 public class Hero extends Actor
 {
-    
+    boolean atTop = true;
     public void act()
     {
-        // Add your action code here.
+        if(Greenfoot.mouseClicked(null))
+        {
+            atTop = !atTop;
+        }
+        
+        if(atTop)
+        {
+            setLocation(100, 100);
+        }
+        else
+        {
+            setLocation(100, 300); 
+        }
     }
 }
