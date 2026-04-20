c2026cd327a60865a7909c96c10dd2dd27672ebe
Kimi Qin <440041588@gapps.yrdsb.ca>
2026-04-20 11:50:42 -0400

    Add change of Marble

--- Marble.java
+++ Marble.java
@@ -6,5 +6,22 @@ public class Marble extends Actor
     public void act()
     {
         move(-2);
+        
+        if(getX() <= 0){
+            resetBanana();
+        }
+    }
+    
+    public void resetBanana()
+    {
+        int x = Greenfoot.getRandomNumber(2);
+        if(x == 0){
+            setLocation(600, 100);
+        }
+        else
+        {
+            setLocation(600, 300);
+        }
+    
     }
 }
