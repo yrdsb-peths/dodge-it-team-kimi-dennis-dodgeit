19c7130d4037b6f078f56c05c41ffb9f18e00d1c
Kimi Qin <440041588@gapps.yrdsb.ca>
2026-04-20 11:31:48 -0400

    Add marble

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,14 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+
+    
+    
+        Hero hero = new Hero();
+        addObject(hero,200,200);
+        
+        Marble marble = new Marble();
+        addObject(marble,500,200);
+
     }
 }
