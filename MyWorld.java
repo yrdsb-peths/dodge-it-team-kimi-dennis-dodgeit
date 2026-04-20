350bf3aed96305eb503144f2d69a48cdbdd371f7
Denis126-dot <denishui126@gmail.com>
2026-04-20 11:56:06 -0400

    Add marble and hero with the code the cn contronl the hero up and down

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,11 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Hero h = new Hero();
+        addObject(h, 200, 200);
+        
+        Marble m = new Marble();
+        addObject(m, 400 , 200);
     }
 }
