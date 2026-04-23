940ef7fee996c58150357c74816b9a7af127553c
Kimi Qin <440041588@gapps.yrdsb.ca>
2026-04-23 11:16:42 -0400

    Add Hp

---
+++ Hp.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Hp here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hp extends Actor
+{
+    /**
+     * Act - do whatever the Hp wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -3,8 +3,6 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
-
-    
     
         Hero hero = new Hero();
         addObject(hero,100,300);
@@ -12,5 +10,11 @@ public class MyWorld extends World {
         Marble marble = new Marble();
         addObject(marble,500,300);
         
+        Hp hp1 = new Hp();
+        addObject(hp1,550,50);
+        Hp hp2 = new Hp();
+        addObject(hp2,500,50);
+        Hp hp3 = new Hp();
+        addObject(hp3,450,50);
     }
 }
